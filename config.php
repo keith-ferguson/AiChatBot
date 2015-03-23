f087242951466af5f9d3b7e142c3bea2082b1c4f
Keith Ferguson <keith.ferguson@student.ncirl.ie>
2015-03-23 18:54:36 +0000

    config file set up

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+# what is your Mysql database server
+# most common is "localhost". Yahoo Small Business hosting uses "mysql"
+$database_host = "ec2-54-163-228-58.compute-1.amazonaws.com";
+
+# what is the name of the database we are using
+$database_name = "del5bhlb9ibhvn";
+
+# who do we log in as?
+$database_user = "cjolxaecmqoitr";
+
+# and what password do we use
+$database_password = '7nstMoDkTdSMZGkdioEI8itAp8';
+
+
+	
+?>
