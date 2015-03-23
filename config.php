0bc0c04abffd57d3c8748649121a36b9b3fa0936
Keith Ferguson <keith.ferguson@student.ncirl.ie>
2015-03-23 18:56:14 +0000

    Ignore for local config

--- config.php
+++ config.php
@@ -1,17 +1,15 @@
 <?php
 # what is your Mysql database server
 # most common is "localhost". Yahoo Small Business hosting uses "mysql"
-$database_host = "ec2-54-163-228-58.compute-1.amazonaws.com";
+$database_host = "localhost";
 
 # what is the name of the database we are using
-$database_name = "del5bhlb9ibhvn";
+$database_name = "aichatbot";
 
 # who do we log in as?
-$database_user = "cjolxaecmqoitr";
+$database_user = "root";
 
 # and what password do we use
-$database_password = '7nstMoDkTdSMZGkdioEI8itAp8';
+$database_password = '';
 
-
-	
 ?>
